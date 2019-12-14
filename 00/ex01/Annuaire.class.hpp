28fa675d8d97e11a5062f2ae94358a59855742f1
Nicolas COLOMER <ncolomer@e2r2p4.42.fr>
2019-12-14 17:55:16 +0100

    Add headers to Annuaire class definition in ex01

--- 00/ex01/Annuaire.class.hpp
+++ 00/ex01/Annuaire.class.hpp
@@ -6,13 +6,16 @@
 /*   By: ncolomer <ncolomer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/14 16:08:49 by ncolomer          #+#    #+#             */
-/*   Updated: 2019/12/14 16:36:37 by ncolomer         ###   ########.fr       */
+/*   Updated: 2019/12/14 17:53:39 by ncolomer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ANNUAIRE_CLASS_HPP
 # define ANNUAIRE_CLASS_HPP
 
+# include <iostream>
+# include <iomanip>
+# include <string>
 #include "Contact.class.hpp"
 
 class Annuaire
