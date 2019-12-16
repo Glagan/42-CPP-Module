b6bda06fcb4727ccd93e6069fc94fabfad41c91c
Nicolas COLOMER <ncolomer@e2r2p4.42.fr>
2019-12-16 18:06:52 +0100

    Add 02 ex00

---
+++ 02/ex00/Fixed.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncolomer <ncolomer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/16 17:14:24 by ncolomer          #+#    #+#             */
+/*   Updated: 2019/12/16 17:57:29 by ncolomer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int Fixed::bits = 8;
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->value = 0;
+}
+
+Fixed::Fixed(Fixed const &other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	this->value = other.getRawBits();
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(Fixed const &other)
+{
+	std::cout << "Assignation operator called" << std::endl;
+	this->value = other.getRawBits();
+	return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->value = raw;
+}
---
+++ 02/ex00/Fixed.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncolomer <ncolomer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/16 17:14:08 by ncolomer          #+#    #+#             */
+/*   Updated: 2019/12/16 17:55:22 by ncolomer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+private:
+	int value;
+	static const int bits;
+public:
+	Fixed();
+	Fixed(Fixed const &other);
+	~Fixed();
+
+	Fixed &operator=(Fixed const &other);
+
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+};
+
+#endif
