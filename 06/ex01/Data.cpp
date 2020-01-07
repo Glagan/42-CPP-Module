b0783db14dd24fec75a4d84cfc3dfc541b05586e
Nicolas COLOMER <ncolomer@e2r2p4.42.fr>
2020-01-07 12:20:58 +0100

    Use reinterpret_cast in 06 ex01

--- 06/ex01/Data.cpp
+++ 06/ex01/Data.cpp
@@ -6,7 +6,7 @@
 /*   By: ncolomer <ncolomer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/27 17:36:08 by ncolomer          #+#    #+#             */
-/*   Updated: 2020/01/07 11:48:10 by ncolomer         ###   ########.fr       */
+/*   Updated: 2020/01/07 12:20:32 by ncolomer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void *serialize(void)
 	raw = new char[20];
 	for (int i = 0; i < 8; i++)
 		raw[i] = alphabet[rand() % alphabetLength];
-	*reinterpret_cast<int*>(raw + 8) = rand();
+	*reinterpret_cast<int*>(raw + 8) = rand() % 424242;
 	for (int i = 0; i < 8; i++)
 		raw[i + 12] = alphabet[rand() % alphabetLength];
 	return (raw);
@@ -34,7 +34,7 @@ Data *deserialize(void *raw)
 	Data *d = new Data;
 
 	d->s1 = std::string(static_cast<char*>(raw), 8);
-	d->n = *reinterpret_cast<int*>(static_cast<char*>(raw) + 12);
+	d->n = *reinterpret_cast<int*>(static_cast<char*>(raw) + 8);
 	d->s2 = std::string(static_cast<char*>(raw) + 12, 8);
 	return (d);
 }
